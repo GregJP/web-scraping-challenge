e0420b4e4cafd552caf0515fb7f358b0c66b26fa
Greg Parker <gregjp91@gmail.com>
2021-03-01 23:34:09 -0500

    Adding and completing app.py

---
+++ Missions_to_Mars/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+# Flask app
+app = Flask(__name__)
+
+# Mongo connection
+mongo = PyMongo(app, uri='mongodb://localhost:27017/mars_app')
+
+# Root route
+@app.route('/')
+def index():
+    mars_scrape = mongo.db.mars_scrape.find_one()
+    return render_template('index.html', mars_scrape=mars_scrape)
+
+# Scrape route
+@app.route('/scrape')
+def scrape():
+    mars_scrape = mongo.db.mars_scrape
+    mars_data = scrape_mars.scrape()
+    mars_scrape.update({}, mars_data, upsert=True)
+
+    return redirect('/', code=302)
+
+if __name__ == '__main__':
+    app.run(debug=True)
